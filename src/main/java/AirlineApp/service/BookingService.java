92f1a9ac41e6cce93b5cefddf0b13983d7925823
OlaGreat <oladipupoolamilekan2@gmail.com>
2023-12-20 23:37:25 +0100

    update

--- src/main/java/AirlineApp/service/BookingService.java
+++ src/main/java/AirlineApp/service/BookingService.java
@@ -1,4 +1,10 @@
 package AirlineApp.service;
 
+
+import AirlineApp.dtos.request.BookingRequest;
+import AirlineApp.dtos.response.BookingResponse;
+
 public interface BookingService {
+    BookingResponse bookFlight(BookingRequest request, String userId);
+
 }
