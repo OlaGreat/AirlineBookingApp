e1797747317bfb4180d84febb4fe16829b51d374
OlaGreat <oladipupoolamilekan2@gmail.com>
2024-01-14 23:38:34 +0100

    added authorizationFliter in the security package

--- src/main/java/AirlineApp/security/services/AirlineUserDetailsService.java
+++ src/main/java/AirlineApp/security/services/AirlineUserDetailsService.java
@@ -1,5 +1,7 @@
 package AirlineApp.security.services;
 
+import AirlineApp.data.models.User;
+import AirlineApp.exceptions.UserNotFoundException;
 import AirlineApp.service.PassengerService;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -15,9 +17,12 @@ public class AirlineUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        
+        try {
+            User userDetails = passengerService.findByEmail(username);
 
-
+        } catch (UserNotFoundException e) {
+            throw new RuntimeException(e);
+        }
 
 
         return null;
